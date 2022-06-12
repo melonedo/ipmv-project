0f122182f50b98dfce2268be1556aea65f9c5109
AuperAng <1275221324@qq.com>
2022-06-12 20:13:56 +0800

    修改校正文件，修改refine

--- src/stereo_rectification.cpp
+++ src/stereo_rectification.cpp
@@ -1,6 +1,8 @@
 #include <math.h>
 
 #include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
